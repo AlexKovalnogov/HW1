2616af0e1e3140787797a1e8c17b879349bb8ebf
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2018-12-04 17:35:22 +0200

    added log4j

--- src/main/java/com/company/MaxSpeed.java
+++ src/main/java/com/company/MaxSpeed.java
@@ -1,16 +1,21 @@
 package com.company;
 
+import org.apache.log4j.Logger;
 
-import sun.rmi.runtime.Log;
+
+
 
 public class MaxSpeed {
+ static   Logger logger = Logger.getLogger(MaxSpeed.class);
 
-private double SpeedKmPerHour=0;
-private double SpeedMetersPerSecond=0;
+    private double SpeedKmPerHour = 0;
+    private double SpeedMetersPerSecond = 0;
+   // static Logger logger = Logger.getLogger(MaxSpeed.class);
 
-public double getSpeedKmPerHour() {
+    public double getSpeedKmPerHour() {
         return SpeedKmPerHour;
     }
+
     public void setSpeedKmPerHour(double speedKmPerHour) {
         SpeedKmPerHour = speedKmPerHour;
     }
@@ -23,69 +28,71 @@ public double getSpeedKmPerHour() {
         SpeedMetersPerSecond = speedMetersPerSecond;
     }
 
-  public   void CompareSpeed(){
+    public void CompareSpeed() {
 
-    int i=0;
+        int i = 0;
         System.out.print("Speed KM/h ");
- setSpeedKmPerHour(ConsoleInputRead.inputAndReadDataFromConsole());
+        setSpeedKmPerHour(ConsoleInputRead.inputAndReadDataFromConsole());
         System.out.print("Speed M/s ");
- setSpeedMetersPerSecond(ConsoleInputRead.inputAndReadDataFromConsole());
-        double SpeedTrasnsform=SpeedKmPerHour*0.27777777;
+        setSpeedMetersPerSecond(ConsoleInputRead.inputAndReadDataFromConsole());
+        double SpeedTrasnsform = SpeedKmPerHour * 0.27777777;
 
-        double MaxValue=0;
+        double MaxValue = 0;
 
-        if(SpeedKmPerHour<0||SpeedMetersPerSecond<0 ){
+        if (SpeedKmPerHour < 0 || SpeedMetersPerSecond < 0) {
             System.out.println("Speed can;t be less 0");
-        }else if (SpeedTrasnsform<SpeedMetersPerSecond)
-        {
-            MaxValue=SpeedMetersPerSecond;
-            i=1;
+            logger.info("Speed can;t be less 0");
+        } else if (SpeedTrasnsform < SpeedMetersPerSecond) {
+            MaxValue = SpeedMetersPerSecond;
+            i = 1;
+
+        } else {
+            MaxValue = SpeedKmPerHour;
+            i = 2;
         }
-        else{
-            MaxValue=SpeedKmPerHour;
-            i=2;
-        }
-        String s="";
-        switch (i){
-            case 1: s ="Km/h"; break;
-            case 2:s ="M/h"; break;
+        String s = "";
+        switch (i) {
+            case 1:
+                s = "Km/h";
+                break;
+            case 2:
+                s = "M/h";
+                break;
         }
 
 
-      System.out.println("Max Speed = "+MaxValue+s );
+        System.out.println("Max Speed = " + MaxValue + s);
 
     }
 
 
-   public double CompareSpeed(int SpeedKmPerHour,int SpeedMetersPerSecond){
+    public double CompareSpeed(int SpeedKmPerHour, int SpeedMetersPerSecond) {
 
 
-        double SpeedTrsnsform=SpeedKmPerHour*0.27777777;
+        double SpeedTrsnsform = SpeedKmPerHour * 0.27777777;
 
-        double MaxValue=0;
+        double MaxValue = 0;
 
-        if(SpeedKmPerHour<0||SpeedMetersPerSecond<0 ){
+        if (SpeedKmPerHour < 0 || SpeedMetersPerSecond < 0) {
             System.out.println("Speed can;t be less 0");
-        }else if (SpeedTrsnsform<SpeedMetersPerSecond)
-        {
-            MaxValue=SpeedMetersPerSecond;
-            System.out.println( MaxValue);
-        }
-        else{
-            MaxValue=SpeedKmPerHour;
-            System.out.println( MaxValue);
+            logger.info("Speed can;t be less 0 from logger");
+        } else if (SpeedTrsnsform < SpeedMetersPerSecond) {
+            MaxValue = SpeedMetersPerSecond;
+            System.out.println(MaxValue);
+        } else {
+            MaxValue = SpeedKmPerHour;
+            System.out.println(MaxValue);
 
         }
-        return  MaxValue;
+        return MaxValue;
 
     }
+
     public static void main(String[] args) {
         MaxSpeed maxSpeed = new MaxSpeed();
-        maxSpeed.CompareSpeed(-100,10);
+        maxSpeed.CompareSpeed(-100, 10);
 
     }
-
-
 
 
 }
