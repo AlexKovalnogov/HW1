c0ba9f2fe13ab4418bfb4da2d8d3ded82bfe8f2d
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2018-12-04 17:47:22 +0200

    Fixing CheckType triangle Test

--- src/test/java/com/company/CheckTypeOfTriangleTest.java
+++ src/test/java/com/company/CheckTypeOfTriangleTest.java
@@ -1,41 +1,37 @@
 package com.company;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CheckTypeOfTriangleTest {
+    CheckTypeOfTriangle checkTypeOfTriangle;
 
-
-
+    @Before
+    public void setUp() throws Exception {
+        checkTypeOfTriangle = new CheckTypeOfTriangle();
+    }
 
     @Test
     public void checkTriangleCase1() {
-        CheckTypeOfTriangle checkTypeOfTriangle = new CheckTypeOfTriangle();
-      assertEquals("Length of Side can't be with minus",checkTypeOfTriangle.checkTriangle(3,5,-1));
-
+        assertEquals("Length of Side can't be with minus", checkTypeOfTriangle.checkTriangle(3, 5, -1));
     }
 
     @Test
     public void checkTriangleCase2() {
-        CheckTypeOfTriangle checkTypeOfTriangle = new CheckTypeOfTriangle();
-        assertEquals("Triangle doesn't exsist",checkTypeOfTriangle.checkTriangle(3,3,7));
-
+        assertEquals("Triangle doesn't exsist", checkTypeOfTriangle.checkTriangle(3, 3, 7));
     }
 
     @Test
     public void checkTriangleCase3() {
-        CheckTypeOfTriangle checkTypeOfTriangle = new CheckTypeOfTriangle();
-        assertEquals("Triangle is right angled",checkTypeOfTriangle.checkTriangle(3,4,5));
+        assertEquals("Triangle is right angled", checkTypeOfTriangle.checkTriangle(3, 4, 5));
     }
 
 
     @Test
     public void checkTriangleCase4() {
-        CheckTypeOfTriangle checkTypeOfTriangle = new CheckTypeOfTriangle();
-        assertEquals("Triangle is not right angled",checkTypeOfTriangle.checkTriangle(3,4,6));
-
+        assertEquals("Triangle is not right angled", checkTypeOfTriangle.checkTriangle(3, 4, 6));
     }
-
 
 }
